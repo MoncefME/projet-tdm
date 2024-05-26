47df7d873de03f7f0c7dbff9433e072a47a9e5ab
MoncefME <ka_moussaoui@esi.dz>
2024-05-26 15:50:30 +0100

    fixing conflicts

--- app/src/main/java/com/example/projettdm/parking_list/presentation/ParkingListScreen.kt
+++ app/src/main/java/com/example/projettdm/parking_list/presentation/ParkingListScreen.kt
@@ -15,6 +15,7 @@ import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Button
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
@@ -34,6 +35,7 @@ import com.example.projettdm.R
 import com.example.projettdm.common.navigation.Screens
 import com.example.projettdm.parking_list.data.remote.ParkingAPI
 import com.example.projettdm.parking_list.data.remote.response.Parking
+import com.example.projettdm.parking_list.presentation.components.ParkingItemCard
 import com.example.projettdm.reservation.presentation.ParkingDetailsViewModel
 
 @SuppressLint("UnrememberedMutableState")
@@ -42,78 +44,27 @@ fun ParkingListScreen(
     navController: NavController, viewModel: ParkingViewModel
 ) {
     Column(
-        modifier = Modifier.fillMaxWidth()
-    ) {
-        DisplayError(viewModel = viewModel)
-        Text(text = "Parking List Screen new")
-        LazyColumn {
-
-            items(viewModel.parkings.value) { parking ->
-                ParkingCard(parking = parking, navController = navController)
-            }
-            items(viewModel.parkings.value) { parking ->
-                ParkingCard(parking = parking, navController = navController)
-            }
-        }
-
-    }
-}
-
-@Composable
-fun ParkingCard(parking: Parking, navController: NavController) {
-    Box(
         modifier = Modifier
             .fillMaxWidth()
-            .padding(16.dp)
-            .background(color = Color(0xFF977897), shape = RoundedCornerShape(16.dp))
+            .padding(horizontal = 8.dp)
     ) {
-        Row(
-            modifier = Modifier
-                .fillMaxWidth()
-                .padding(16.dp)
-        ) {
-            Image(
-                painter = painterResource(id = R.drawable.parking),
-                contentDescription = "Image of the parking",
-                modifier = Modifier
-                    .size(150.dp)
-                    .padding(end = 16.dp)
-            )
+        Text(
+            text = "Parking List",
+            style = MaterialTheme.typography.headlineMedium,
+            fontWeight = FontWeight.Bold,
+            color = MaterialTheme.colorScheme.onSurface,
+            modifier = Modifier.padding(vertical = 8.dp)
+        )
 
-            Column(
-                modifier = Modifier.weight(1f)
-            ) {
-                Text(text = parking.id.toString(), fontSize = 16.sp, modifier = Modifier.padding(bottom = 8.dp))
-                Text(
-                    text = parking.name,
-                    fontWeight = FontWeight.Bold,
-                    fontSize = 20.sp,
-                    modifier = Modifier.padding(bottom = 8.dp)
-                )
-                Text(
-                    text = parking.city,
-                    fontSize = 16.sp,
-                    modifier = Modifier.padding(bottom = 8.dp)
-                )
-                Text(
-                    text = parking.price.toString(),
-                    fontSize = 16.sp,
-                    modifier = Modifier.padding(bottom = 8.dp)
-                )
-                Button(
-                    modifier = Modifier.background(color = Color( 0xfffff), shape = RoundedCornerShape(16.dp)),
-                    onClick = {
-                        navController.navigate(Screens.ParkingDetailsScreen.route + "/${parking.id.toString()}")
-                    },
-//                    modifier = Modifier.align(LineHeightStyle.Alignment.End)
-                ) {
-                    Text(text = "Details ${parking.id.toString()}")
-                }
+        DisplayError(viewModel = viewModel)
+
+        LazyColumn {
+            items(viewModel.parkings.value) { parking ->
+                ParkingItemCard(parking = parking, navController = navController)
             }
         }
     }
 }
-
 
 @Composable
 fun DisplayError(viewModel: ParkingViewModel){
