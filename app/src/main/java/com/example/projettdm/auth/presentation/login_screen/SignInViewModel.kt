3f8535f32c5a57d689125131cea426c75c4c7b7a
serinesefardjelah <ks_sefardjelah@esi.dz>
2024-06-10 18:05:42 +0100

    solved conflicts

--- app/src/main/java/com/example/projettdm/auth/presentation/login_screen/SignInViewModel.kt
+++ app/src/main/java/com/example/projettdm/auth/presentation/login_screen/SignInViewModel.kt
@@ -30,8 +30,8 @@ class SignInViewModel @Inject constructor(
     val signInState: Flow<SignInState> = _signInState.receiveAsFlow()
 
 
-    fun loginUser(email: String, password: String) = viewModelScope.launch {
-            repository.login(email, password).let { response ->
+    fun loginUser(email: String, password: String, fcmToken: String) = viewModelScope.launch {
+            repository.login(email, password, fcmToken).let { response ->
                 when(response){
                     is Resource.Success -> {
                         _signInState.send(SignInState(isSuccess = true))
